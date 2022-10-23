28e1640ffc9e661d7b80778a8466c29fc7cf0371
Dmytro Tryfiak (Astoundcommerce) <d.tryfiak@astoundcommerce.com>
2022-10-23 15:21:23 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryContent = galleryItems.reduce( (galleryContent,item) => {
+    return galleryContent += 
+        `<a class="gallery__item" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}"
+            />
+        </a>`    
+}, '');
+
+gallery.insertAdjacentHTML('afterbegin', galleryContent);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
 
 console.log(galleryItems);
