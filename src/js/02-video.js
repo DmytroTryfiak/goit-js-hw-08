1d40378c20d672f88150485646eb5e02798664fd
Dmytro Tryfiak (Astoundcommerce) <d.tryfiak@astoundcommerce.com>
2022-10-23 19:42:37 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', onPlay);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onPlay(data) {
+    const videoplayerCurrentTime = localStorage.getItem("videoplayer-current-time");
+    if (videoplayerCurrentTime && videoplayerCurrentTime < data.duration) {
+        player.setCurrentTime(videoplayerCurrentTime);
+        return;
+    } 
+    if (videoplayerCurrentTime) {
+        localStorage.removeItem("videoplayer-current-time");
+    }
+};
+
+function  onTimeUpdate (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}
